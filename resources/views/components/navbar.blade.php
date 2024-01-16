783025d1ff8e3020d37fce00f132e49cec666ce4
Conca <conca@contoso.com>
2024-01-16 20:16:59 +0100

    Add Navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="bg-slate-900 flex py-3">
+    <div class="w-3/4 mx-auto text-slate-50 flex items-center">
+        <a href="/" class="me-16">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-8 h-8 inline align-text-bottom me-2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+            </svg>
+            <span class="text-3xl font-semibold align-bottom">Archive</span>
+        </a>
+        <ul class="flex gap-8 [&>li>a]:transition-all [&>li>a:hover]:text-slate-400">
+            <li><a href="/">Home</a></li>
+            <li><a href="/users">Users</a></li>
+        </ul>
+    </div>
+</nav>
